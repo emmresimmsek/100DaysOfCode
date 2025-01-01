a0eb27691ba7e0d0ed4f59e53127e3bf816c6c1a
Emre Şimşek <emmresimmsek@gmail.com>
2025-01-01 03:59:59 +0300

    ilk soruyu çözdüm.

--- src/Day1.java
+++ src/Day1.java
@@ -1,7 +1,13 @@
 public class Day1 {
     public static void main(String[] args) {
-  // sfsdfsdfsdf
 
+        // 1. Write a program that prints "Hello World!" to the console
+        System.out.println("Hello World!"); // Konsola mesajı yazdır
+
+        // 2. Add single line and multi-line comments to your code
+
+        /* Multi Line Comments */
+        // Single Line Comments
 
 
     }
