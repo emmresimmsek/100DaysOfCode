4586fa1929e30435904cb30c918efea70ac44a76
Emre Şimşek <emmresimmsek@gmail.com>
2025-01-02 05:06:00 +0300

    100 Days of Java challenges

--- src/Day2.java
+++ src/Day2.java
@@ -2,6 +2,21 @@ public class Day2 {
     public static void main(String[] args) {
 
 
+        /* 1. Declare and initialize variables for various data types including integer (int),
+        floating-point number (double), and string (String). Then, print each variable to the console.*/
+
+        int myInteger = 42;
+        double myDouble = 3.14;
+        String myString = "Hello, World!";
+
+        // Print the variables to the console
+        System.out.println("Integer: " + myInteger);
+        System.out.println("Double: " + myDouble);
+        System.out.println("String: " + myString);
+
+
+
+
 
 
 
